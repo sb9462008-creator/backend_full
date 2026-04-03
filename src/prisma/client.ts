88ff270579447a8653c9f52ac3c925ccf7fe2b13
XeKu <sb9462008@gmail.com>
2026-04-03 13:05:16 +0800

    Update client.ts

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -8,6 +8,10 @@ export const prisma =
   global.prisma ??
   new PrismaClient({
     log: ["error", "warn"],
+    transactionOptions: {
+      timeout: 30000,
+      maxWait: 10000,
+    },
   });
 
 if (process.env.NODE_ENV !== "production") {
